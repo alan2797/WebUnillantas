6a414c3143cee8bb64d402e26b320ab8d3c4cb63
vladimiralan <vasquezvladimir169@gmail.com>
2025-11-25 18:17:17 -0400

    feat: notificaciones

--- src/utils/handle-request-axios.ts
+++ src/utils/handle-request-axios.ts
@@ -1,6 +1,6 @@
 import { type AppDispatch } from "../redux/store";
 import { showSpinner, hideSpinner } from "../redux/features/spinner.slice";
-import { message } from "antd";
+import { message, notification } from "antd";
 import type { AxiosResponse } from "axios";
 import type { ApiResponse, HandleOptions } from "../interfaces/components.interface";
 import { logout } from "../redux/features/auth.slice";
@@ -20,17 +20,17 @@ export async function handleRequestAxios<T>(
     const result = response.data;
 
     if (result?.success) {
-      if (successMessage) message.success(successMessage);
-      if (showMessageApi && result.message) message.success(result.message);
+      if (successMessage) notification.success({ message: successMessage, duration: 5, placement: "top" });
+      if (showMessageApi && result.message) notification.success({ message: result.message, duration: 5, placement: "top" });
     } else {
-      if (errorMessage) message.error(errorMessage);
+      if (errorMessage) notification.error({ message: errorMessage, duration: 5, placement: "top" });
       if (showMessageApi && (result?.message || (result?.error && typeof result?.error === "string"))) {
-        message.error(result.message || String(result?.error));
+        notification.error({ message: result.message || String(result?.error), duration: 5, placement: "top" });
       }
     }
 
     return result;
-  } catch (err: any) {    
+  } catch (err: any) {
     const status = err.response?.status;
     const serverMessage = err.response?.data?.message;
     const serverErrors = err.response?.data?.error?.errors;
@@ -41,7 +41,7 @@ export async function handleRequestAxios<T>(
       serverMessage,
       errorMessage,
       serverErrors,
-      onUnauthorized: () => dispatch(logout())
+      onUnauthorized: () => dispatch(logout()),
     });
 
     return null;
